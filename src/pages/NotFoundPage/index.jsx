2e2c51df941cc1f3b7418b7a81158a2340453a66
Muhammad Avicena <cenarahmant.dev@gmail.com>
2024-02-23 02:57:15 +0700

    feat: modify NotFoundPage

--- src/pages/NotFoundPage/index.jsx
+++ src/pages/NotFoundPage/index.jsx
@@ -1,9 +1,26 @@
+import { TERipple } from "tw-elements-react";
+
 function NotFoundPage() {
   return (
-    <div>
-      <h1 className="block text-gray-700 text-sm font-bold mb-2">
-        Not Found !
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold text-white font-bold mb-4">
+        404 Not Found
       </h1>
+      <p className="text-gray-500">
+        Oops! The page you are looking for does not exist.
+      </p>
+
+      <TERipple rippleColor="light">
+        <button
+          type="submit"
+          onClick={() => {
+            window.location.href = "/";
+          }}
+          className="rounded bg-primary px-5 py-2 my-2 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+        >
+          Go Back
+        </button>
+      </TERipple>
     </div>
   );
 }
